d67db73f9a686cc4221bccf6a1a287a5b4143118
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-03-05 21:10:26 -0500

    Added HTTP encoding to strings being sent

--- server/finish-notify.js
+++ server/finish-notify.js
@@ -1,7 +1,10 @@
 var http = require('http');
+var querystring = require('querystring');
+
+var torrentName = querystring.stringify(process.argv[2]);
+var directory = querystring.stringify(process.argv[3]);
 
 var url = 'http://127.0.0.1:8080/finished?name=' + process.argv[2] + '&directory=' + process.argv[3];
-console.log(url);
 
 http.get(url, function(res) {
 	console.log('Server responded with ' + res.statusCode);
